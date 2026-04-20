47c39ec98705d6bf891178ff8c1c7ec687505f87
Ashish-Rawat7 <rawatashishsingh12@gmail.com>
2026-04-20 12:27:24 +0530

    Update the code

--- Chatbot/grammar.py
+++ Chatbot/grammar.py
@@ -1,21 +1,3 @@
-# import language_tool_python
-
-# tool = language_tool_python.LanguageTool('en-US')
-
-# def correct_grammar(text):
-#     matches = tool.check(text)
-#     corrected = language_tool_python.utils.correct(text, matches)
-
-#     explanations = []
-#     for match in matches:
-#         explanations.append({
-#             "message": match.message,
-#             "suggestions": match.replacements[:2]
-#         })
-
-#     return corrected, explanations
-
-
 import os
 
 ENABLE_GRAMMAR = os.getenv("ENABLE_GRAMMAR", "True").lower() == "true"
